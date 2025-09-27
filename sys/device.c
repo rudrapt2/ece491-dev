b871d23abaad1d2b49849640fe0cd1438f434bdd
Michael Vincent <mhvincent.2604@gmail.com>
2025-09-27 15:20:06 -0500

    device uio implemenentations

--- sys/device.c
+++ sys/device.c
@@ -52,6 +52,13 @@ struct video_uio {
     // ...
 };
 
+struct storage_uio {
+    struct uio base;
+    struct storage * sto;
+    unsigned long pos;
+
+}
+
 // INTERNAL FUNCTION DECLARATIONS
 //
 
@@ -482,26 +489,47 @@ long serial_uio_write(struct uio * uio, const void * buf, unsigned long buflen) 
 }
 
 int storage_open_uio(struct storage * sto, struct uio ** uioptr) {
-    return -ENOTSUP;
+    struct storage_uio * suio;
+    int result;
+
+    // Try to open device
+
+    result = storage_open(sto);
+
+    if (result != 0)
+        return result;
+    
+    suio = kcalloc(1, sizeof(*suio));
+
+    suio->sto = sto;
+    *uioptr = uio_init1(&suio->base, &storage_uio_intf);
+    return 0;
+    
 }
 
 void storage_uio_close(struct uio * uio) {
-    // ...
+    struct storage_uio * suio = (struct storage_uio*)uio;
+    storage_close(suio->sto);
+
+    kfree(suio);
 }
 
 long storage_uio_read(struct uio * uio, void * buf, unsigned long bufsz) {
-    // ...
-    return -ENOTSUP;
+    struct storage_uio * suio = (struct storage_uio*)uio;
+
+    return storage_fetch(suio->sto, suio->pos, buf, bufsz);
 }
 
 long storage_uio_write(struct uio * uio, const void * buf, unsigned long buflen) {
-    // ...
-    return -ENOTSUP;
+    struct storage_uio * suio = (struct storage_uio*)uio;
+
+    return storage_store(suio->sto, suio->pos, buf, buflen);
 }
 
 int storage_uio_cntl(struct uio * uio, int op, void * arg) {
-    // ...
-    return -ENOTSUP;
+    struct storage_uio * suio = (struct storage_uio*)uio;
+
+    return storage_cntl(suio->sto, op, arg);
 }
 
 int video_open_uio(struct video * vid, struct uio ** uioptr) {
@@ -509,7 +537,6 @@ int video_open_uio(struct video * vid, struct uio ** uioptr) {
 }
 
 void video_uio_close(struct uio * uio) {
-    // ...
 }
 
 long video_uio_write(struct uio * uio, const void * buf, unsigned long buflen) {
