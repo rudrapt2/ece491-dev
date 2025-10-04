7813307f6188614d3766210f47b68774923fd995
vpal2 <vpal2@eceb-3026-04.ews.illinois.edu>
2025-10-04 15:43:02 -0500

    added games branch

--- util/fs/mkfs.sh
+++ util/fs/mkfs.sh
@@ -1,3 +1,3 @@
 make -C ../../usr
 make
-./mkfs_ktfs ../../sys/ktfs.raw 35M 16 rudra_garbo small ../../usr/bin/*
+./mkfs_ktfs ../../sys/ktfs.raw 35M 16 rudra_garbo small ../../usr/bin/* ../../usr/bin/games/*
